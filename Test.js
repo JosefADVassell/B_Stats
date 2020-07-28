b7b64d21498e02b8315d3cd4a81e6e8fb66176d2
Josef Vassell <Josefvassell@outlook.com>
2020-07-28 16:28:28 -0500

    first

--- Test.js
+++ Test.js
@@ -3,6 +3,9 @@
 var btn1 = document.querySelector('#btn-1');
 var btn2 = document.querySelector('#btn-2');
 var btn3 = document.querySelector('#btn-3');
+var btn4 = document.querySelector('#btn-4');
+var number = document.querySelector('#number');
+
 
 
 btn1.addEventListener('click', () =>{
@@ -15,8 +18,17 @@ btn2.addEventListener('click', () =>{
 
 btn3.addEventListener('click', () =>{
      document.body.style.backgroundImage = "url('rivv.jpg')";
-        
+      
 });
+
+btn4.addEventListener("click", () =>{
+    document.body.style.backgroundImage = "url('lightblue.jpg')";
+});
+
+// for total score
+
+
+
 
 function runCode() {
     alert("hello saulito");
@@ -73,9 +85,124 @@ function fouls(id)
     }   
 
 }
+
+// total rebound function
+
 function TotalReb(id)
 {
     incrementValue(id);
-    var value = parseInt(document.getElementById(id).value, 10);  
+    
+    var Dreb_value = parseInt(document.getElementById("D_Rebound").value, 10);  
+    var Oreb_value = parseInt(document.getElementById("O_Rebound").value, 10);  
+    // get defense rebound value and add it to offense rebound to total rebound
+    document.getElementById("Total_Rebound").value = Dreb_value + Oreb_value;
+   
+}
+
+function ThreeFieldGoal(id)
+{   
+    // and if i clcik on 3FGM then 3FGA goes up also
+
+    incrementValue(id);
+    var FGM_number = parseInt(document.getElementById("3FGM_number").value, 10);
+    var FGA_number = parseInt(document.getElementById("3FGA_number").value, 10);
+   
+    if(id==="3FGM_number")
+    {
+      document.getElementById("3FGA_number").value = FGA_number + 1;
+      
+    FGA_number++;
+    //var TP_value = document.getElementById("TP").value = FGM_number+2;
+    update(id);
+    } 
+
+    // get three field goal  percentage 
+  
+    
+   var valuee= document.getElementById("3Percent").value = Math.round((FGM_number) / (FGA_number)*100) +'%';
+
+  
+}
+
+function FieldGoal(id)
+{
+    incrementValue(id);
+    var FGM_number = parseInt(document.getElementById("FGM_number").value, 10);
+    var FGA_number = parseInt(document.getElementById("FGA_number").value, 10);
+   
+    if(id==="FGM_number")
+    {
+      document.getElementById("FGA_number").value = FGA_number + 1;
+    //  var TP_value = document.getElementById("TP").value = FGM_number+1
+    FGA_number++;
+    update(id);
+    } 
+
+    // get three field goal  percentage 
+  
+    
+   var valuee= document.getElementById("2Percent").value = Math.round((FGM_number) / (FGA_number)*100) +'%';
+
+}
+function FreeThrows(id)
+{
+    incrementValue(id);
+    var FTM_number = parseInt(document.getElementById("FTM_number").value, 10);
+    var FTA_number = parseInt(document.getElementById("FTA_number").value, 10);
+   
+    if(id==="FTM_number")
+    {
+      document.getElementById("FTA_number").value = FTA_number + 1;
+    
+    FTA_number++;
+   // var TP_value = document.getElementById("TP").value = FTM_number;
+   update(id);
+    }
+
+
+    // get three field goal  percentage 
+  
+    
+   var valuee= document.getElementById("FTPercent").value = Math.round((FTM_number) / (FTA_number)*100) +'%';
+}
+// update the total score 
+// step 1 get the value id for total points 
+//check if you can change nummber
+// 
+function update(id)
+{
+    var FTM_number = parseInt(document.getElementById("FTM_number").value, 10);
+    //var TP_value = parseInt(document.getElementById("TP").value,10);
+   // var FGM_number = parseInt(document.getElementById("FGM_number").value, 10);
+   // var Three_FGM = parseInt(document.getElementById("3FGM_number").value, 10);
+   
+    //TP_value= document.getElementById("TP").value = FTM_number;
+    //console.log(TP_value);
+
+   if(id === "FTM_number")
+    { 
+    var value =  parseInt(document.getElementById("TP").value,10);
+    value++;
+    document.getElementById("TP").value = value;
+    
+       
+    }
+    if(id === "FGM_number")
+    {
+    var value =  parseInt(document.getElementById("TP").value,10);
+    value= value + 2;
+    document.getElementById("TP").value = value;
+      
+     
+    }
+    if(id ==="3FGM_number")
+    {
+        var value =  parseInt(document.getElementById("TP").value,10);
+        value= value + 3;
+        document.getElementById("TP").value = value;
+          
+    }
     
 }
+
+
